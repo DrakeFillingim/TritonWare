f0a943ebb148dd41c38a1f133561a24069d95eaf
DrakeFillinigm <drake.fillingim@gmail.com>
2024-10-20 15:48:26 -0700

    started wave attack

---
+++ TritonWare Game Jam/Assets/Scripts/Entities/Enemies/WaveAttack.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class WaveAttack : MonoBehaviour
+{
+    private static Sprite _waveSprite = Resources.Load<Sprite>("Sprites/WaveAttack");
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
