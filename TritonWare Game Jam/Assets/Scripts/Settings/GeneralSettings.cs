12b2eda7a7156384cfb24f76af42460a6d682164
DrakeFillinigm <drake.fillingim@gmail.com>
2024-10-22 11:28:00 -0700

    added ability to quit game

--- TritonWare Game Jam/Assets/Scripts/Settings/GeneralSettings.cs
+++ TritonWare Game Jam/Assets/Scripts/Settings/GeneralSettings.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GeneralSettings : MonoBehaviour
 {
+    private InputActionMap _inputMap;
+
     void Start()
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 144;
+    }
+
+    private void OnEnable()
+    {
+        _inputMap = GetComponent<PlayerInput>().actions.FindActionMap("Player");
+        _inputMap["Quit"].performed += OnQuit;
+    }
+
+    private void OnDisable()
+    {
+        _inputMap["Quit"].performed -= OnQuit;
+    }
+
+    private void OnQuit(InputAction.CallbackContext context)
+    {
+        print("here");
+        Application.Quit();
     }
 }
